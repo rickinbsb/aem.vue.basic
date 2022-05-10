cf284e7c4da65a62a29ad7478ec2fbcf7bbc744c
Pedro Palmeira <pedro.palmeira@compasso.com.br>
2022-05-10 02:02:10 -0300

    :sparkles: Login components

---
+++ ui.frontend/src/components/micro/BrandLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="brand">
+    <a :href="homePath + '.html'"><img :src="imagePath" /></a>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    imagePath: { type: String, default: '#' },
+    homePath: { type: String, default: '#' }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#brand {
+  margin: -6px 0 0 -12px;
+
+  img {
+    height: 84px;
+  }
+}
+</style>
